591eb6687f373aa194540687c9912a37f9d8d6f1
GabrielBorges2000 <gabriel.vscode@gmail.com>
2026-06-25 23:18:49 -0300

    feat: add SEO optimization

--- scripts/serve.js
+++ scripts/serve.js
@@ -26,6 +26,7 @@ const MIME = {
   ".mjs": "text/javascript; charset=utf-8",
   ".css": "text/css; charset=utf-8",
   ".json": "application/json; charset=utf-8",
+  ".xml": "application/xml; charset=utf-8",
   ".txt": "text/plain; charset=utf-8",
   ".md": "text/markdown; charset=utf-8",
   ".svg": "image/svg+xml",
